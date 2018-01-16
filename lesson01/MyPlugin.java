fcd889783a0fc324cabbceed9ea1c3059474747a
AmaZiX <amazichannel@gmail.com>
2018-01-16 20:10:34 +0700

    Update MyPlugin.java

--- lesson01/MyPlugin.java
+++ lesson01/MyPlugin.java
@@ -1,6 +1,9 @@
 package me.gtoidz.myplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class MyPlugin extends JavaPlugin {
 
@@ -12,6 +15,15 @@ public class MyPlugin extends JavaPlugin {
   @Override
   public void onDisable(){
     Bukkit.getConsoleSender().sendMessage("MyPlugin has been disabled!");
+  }
+  
+  @Override
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+    Player player = (Player) sender;
+    if(cmd.getName().equalsIgnoreCase("cmd"){
+      player.sendMessage("Hello!");
+    }
+    return false;
   }
   
 }
